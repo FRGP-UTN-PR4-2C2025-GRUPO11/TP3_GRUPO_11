3ca1da99f48b8948964f3287135be3dcfc443546
Mariano Belgrano <mbelgrano@gmail.com>
2025-09-08 00:17:19 -0300

    Paquete dao. Clase DaoProducto. Conexión a BBDD

---
+++ src/dao/DaoProducto.java
@@ -0,0 +1,8 @@
+package dao;
+
+public class DaoProducto {
+	private String host = "jdbc:mysql://localhost:3306/";
+	private String user = "root";
+	private String pass = "root";
+	private String dbName = "bdinventario";
+}
