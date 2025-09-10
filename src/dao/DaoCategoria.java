ff43c8da4d06872ed12b981588f4a26a462f2bfa
CZubileteSanchez <carloszubilete@gmail.com>
2025-09-10 03:23:41 -0300

    Agregamos dos funciones a DaoCategorias: ModificarCategoria y ListarCategoria . Tambien creamos el metodo 'setIdCategoria' en Categoria

--- src/dao/DaoCategoria.java
+++ src/dao/DaoCategoria.java
@@ -3,7 +3,9 @@ package dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 import entidad.Categoria;
 
@@ -12,44 +14,83 @@ public class DaoCategoria {
 	private String user = "root";
 	private String pass = "root";
 	private String dbName = "bdinventario";
-	
+
 	public DaoCategoria() {
-		
+
 	}
-	
+
 	public int agregarCategoria(Categoria categoria) {
-		String query = "INSERT INTO categorias(idCategoria,Nombre) VALUES ('"+categoria.getIdCategoria()+"','"+categoria.getNombre()+"')";
-		
+		String query = "INSERT INTO categorias(idCategoria,Nombre) VALUES ('"
+		    + categoria.getIdCategoria() + "','" + categoria.getNombre() + "')";
+
 		Connection cn = null;
 		int filas = 0;
-		
+
 		try {
-			cn = DriverManager.getConnection(host+dbName,user,pass);
+			cn = DriverManager.getConnection(host + dbName, user, pass);
 			Statement st = cn.createStatement();
 			filas = st.executeUpdate(query);
-		}
-		catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return filas;
 	}
-	
-	public int eliminarCategoria(Categoria categoria) {		
+
+	public int eliminarCategoria(Categoria categoria) {
 		Connection cn = null;
 		int filas = 0;
-		
+
 		try {
-			cn = DriverManager.getConnection(host+dbName,user,pass);
+			cn = DriverManager.getConnection(host + dbName, user, pass);
 			String query = "DELETE FROM categoria WHERE idCategoria = ?";
 			PreparedStatement pst = cn.prepareStatement(query);
 			pst.setInt(1, categoria.getIdCategoria());
 			filas = pst.executeUpdate();
-		}
-		catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return filas;
+	}
+
+	public int modificarCategoria(Categoria categoria) {
+		Connection cn = null;
+		int filas = 0;
+
+		try {
+			cn = DriverManager.getConnection(host+dbName,user,pass);
+			String query = "UPDATE categorias SET Nombre = ? WHERE idCategoria = ?";
+			PreparedStatement pst = cn.prepareStatement(query);
+			pst.setString(1, categoria.getNombre());
+			pst.setInt(2, categoria.getIdCategoria());
+			filas = pst.executeUpdate();
+			}
+			catch(Exception e) {
+				e.printStackTrace();
+			}
+		return filas;
+	}
+
+	public ArrayList<Categoria> listarCategorias(){
+		ArrayList<Categoria> lista = new ArrayList<Categoria>();
+		Connection cn = null;
+		try {
+			cn = DriverManager.getConnection(host+dbName,user,pass);
+			String query = "SELECT * FROM categorias";
+			Statement st = cn.createStatement();
+			ResultSet rs = st.executeQuery(query);
+
+			while(rs.next()) {
+				Categoria c = new Categoria();
+				c.setIdCategoria(rs.getInt("idCategoria"));
+				c.setNombre(rs.getString("Nombre"));
+				lista.add(c);
+			}
+		}	catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		return lista;
 	}
 }
--- src/entidad/Categoria.java
+++ src/entidad/Categoria.java
@@ -3,17 +3,26 @@ package entidad;
 public class Categoria {
 	private int idCategoria;
 	private String nombre;
-	public Categoria ()
-	{}
+
+	public Categoria() {
+	}
+
 	public int getIdCategoria() {
 		return idCategoria;
 	}
+
+	public void setIdCategoria(int idCategoria) {
+		this.idCategoria = idCategoria;
+	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	@Override
 	public String toString() {
 		return "Categoria [idCategoria=" + idCategoria + ", nombre=" + nombre + "]";
